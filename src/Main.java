4c3a9220d329b9113ad821a65a57a30609bf7efe
maxime <max_lech_13@outlook.com>
2020-09-25 17:07:51 -0400

    Prototype fix

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,11 @@
 import java.io.*;
 import java.nio.file.*;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) throws Exception {
 
-        String fichier = "";
         List<File> filesInFolder = Files.walk(Paths.get("codeSource"))
                 .filter(Files::isRegularFile)
                 .map(Path::toFile)
@@ -15,30 +13,23 @@ public class Main {
 
         System.out.println(filesInFolder);
 
-        /*File folder = new File("codeSource");
-        String[] listeFichiers = folder.list();*/
-        for(File path:filesInFolder) {
+        for (File path:filesInFolder) {
             File file = new File(String.valueOf(path));
             BufferedReader br = new BufferedReader(new FileReader(file));
 
             int x = 0;
             String st;
-            while ((st = br.readLine()) != null){
+            while ((st = br.readLine()) != null) {
+                if ("".equals(st) ) {continue;}
+                else if (st.contains("//")) {continue;}
+                else if (st.contains("/*")) {continue;}
+                else if (st.endsWith("*/")) {continue;}
+                System.out.println(x);
                 x++;
             }
             System.out.println(x);
             System.out.println(path);
         }
-
-//        File file = new File("codeSource\\test.java");
-//        BufferedReader br = new BufferedReader(new FileReader(file));
-//
-//        int x = 0;
-//        String st;
-//        while ((st = br.readLine()) != null){
-//            x++;
-//        }
-//        System.out.println(x);
     }
 }
 
